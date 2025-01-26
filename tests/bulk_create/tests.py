dc5394f544d3ac59ad8930b6a3217bead9e2c6cf
Raphael Gaschignard <raphael@rtpg.co>
2025-01-26 14:24:02 +1000

    Codegen coverage for bulk_create

--- tests/bulk_create/tests.py
+++ tests/bulk_create/tests.py
@@ -18,6 +18,7 @@ from django.test import (
     skipIfDBFeature,
     skipUnlessDBFeature,
 )
+from django.utils.codegen import from_codegen, generate_unasynced
 
 from .models import (
     BigAutoFieldModel,
@@ -254,20 +255,39 @@ class BulkCreateTests(TestCase):
             )
             self.assertLess(len(connection.queries), 10)
 
+    @from_codegen
     def test_explicit_batch_size(self):
-        objs = [TwoFields(f1=i, f2=i) for i in range(0, 4)]
-        num_objs = len(objs)
-        TwoFields.objects.bulk_create(objs, batch_size=1)
-        self.assertEqual(TwoFields.objects.count(), num_objs)
-        TwoFields.objects.all().delete()
-        TwoFields.objects.bulk_create(objs, batch_size=2)
-        self.assertEqual(TwoFields.objects.count(), num_objs)
-        TwoFields.objects.all().delete()
-        TwoFields.objects.bulk_create(objs, batch_size=3)
-        self.assertEqual(TwoFields.objects.count(), num_objs)
-        TwoFields.objects.all().delete()
-        TwoFields.objects.bulk_create(objs, batch_size=num_objs)
-        self.assertEqual(TwoFields.objects.count(), num_objs)
+        with new_connection(force_rollback=True):
+            objs = [TwoFields(f1=i, f2=i) for i in range(0, 4)]
+            num_objs = len(objs)
+            TwoFields.objects.bulk_create(objs, batch_size=1)
+            self.assertEqual(TwoFields.objects.count(), num_objs)
+            TwoFields.objects.all().delete()
+            TwoFields.objects.bulk_create(objs, batch_size=2)
+            self.assertEqual(TwoFields.objects.count(), num_objs)
+            TwoFields.objects.all().delete()
+            TwoFields.objects.bulk_create(objs, batch_size=3)
+            self.assertEqual(TwoFields.objects.count(), num_objs)
+            TwoFields.objects.all().delete()
+            TwoFields.objects.bulk_create(objs, batch_size=num_objs)
+            self.assertEqual(TwoFields.objects.count(), num_objs)
+
+    @generate_unasynced()
+    async def test_async_explicit_batch_size(self):
+        async with new_connection(force_rollback=True):
+            objs = [TwoFields(f1=i, f2=i) for i in range(0, 4)]
+            num_objs = len(objs)
+            await TwoFields.objects.abulk_create(objs, batch_size=1)
+            self.assertEqual(await TwoFields.objects.acount(), num_objs)
+            await TwoFields.objects.all().adelete()
+            await TwoFields.objects.abulk_create(objs, batch_size=2)
+            self.assertEqual(await TwoFields.objects.acount(), num_objs)
+            await TwoFields.objects.all().adelete()
+            await TwoFields.objects.abulk_create(objs, batch_size=3)
+            self.assertEqual(await TwoFields.objects.acount(), num_objs)
+            await TwoFields.objects.all().adelete()
+            await TwoFields.objects.abulk_create(objs, batch_size=num_objs)
+            self.assertEqual(await TwoFields.objects.acount(), num_objs)
 
     def test_empty_model(self):
         NoFields.objects.bulk_create([NoFields() for i in range(2)])
